71571aa9f0580d1a691df28619ece24d379da615
Elise St Hilaire <eliseylenore@gmail.com>
2017-01-13 10:39:15 -0800

    Add variables to grab answer for each question

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,11 @@ $(document).ready(function(){
   $("form").submit(function(event){
     $("hideMe").hide();
     event.preventDefault();
-    var design = $("input:radio[name:design]:checked");
-    var businessSize = $("input:radio[name:business-size]:checked");
-    var workType = $("input:radio[name:work-type]:checked");
-    var future = $("input:radio[name:future]:checked");
-    var system = $("input:radio[name:system]:checked");
+    var design = $("input:radio[name=design]:checked").val();
+    var businessSize = $("input:radio[name=business-size]:checked").val();
+    var workType = $("input:radio[name=work-type]:checked").val();
+    var future = $("input:radio[name=future]:checked").val();
+    var windowsVsMac = $("input:radio[name=windows-vs-mac]:checked").val();
+    console.log(windowsVsMac);
   });
 });
