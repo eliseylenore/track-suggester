4ec24ae062c5989eec2b1237efd1eccfb3fa3178
Elise St Hilaire <eliseylenore@gmail.com>
2017-01-13 11:00:01 -0800

    Add branching to determine which track a user should take based on their answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 $(document).ready(function(){
   $("form").submit(function(event){
-    $("hideMe").hide();
+    $(".hideMe").hide();
     event.preventDefault();
     var design = $("input:radio[name=design]:checked").val();
     var businessSize = $("input:radio[name=business-size]:checked").val();
     var workType = $("input:radio[name=work-type]:checked").val();
     var future = $("input:radio[name=future]:checked").val();
     var windowsVsMac = $("input:radio[name=windows-vs-mac]:checked").val();
-    console.log(windowsVsMac);
+    console.log(businessSize);
+
+    if (design === "yes" && future === "cocktails") {
+      $("#css-design").show();
+    } else if (businessSize === "medium" && workType === "agency") {
+      $("#php-drupal").show();
+    } else if (businessSize === "large" && worktype === "android dev") {
+      $("#java-android").show();
+    } else if (businessSize === "large" && windowsVsMac === "windows" || workType === "business software") {
+      $("#cnet").show();
+    } else if (businessSize === "small") {
+      $("#ruby-rails").show();
+    }
   });
 });
